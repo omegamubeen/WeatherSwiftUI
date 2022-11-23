295a08b5638cd04db44cfb76bdcd2cd53d25c893
Mubeen Khalid <mubeenjutt005@gmail.com>
2022-11-23 16:04:59 +0500

    Initial Commit

---
+++ WeatherPractice/WeatherPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherPracticeApp.swift
+//  WeatherPractice
+//
+//  Created by Mubeen Khalid on 23/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
